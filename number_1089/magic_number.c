5886eb19eaeca145364f52c16ff07f7340800457
galupo01 <115595096+galupo01@users.noreply.github.com>
2022-10-11 21:36:44 -0300

    Update magic_number.c
    To be considered a magic number the final result of all operations has to be 1089, and it has to have 3 digits. Firstly, you take away the number with itself inverted, get the result and add with the result inverted, and know, the final result has to be 1089. Example: 321 - 123 = 198, 198 + 891 = 1089, so 321 is a magic number. The objective of this code is to show all of the magic numbers (504).

--- number_1089/magic_number.c
+++ number_1089/magic_number.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main(void) {
-  int n, ninv, x, xinv, y, caso = 0, c1, u1, d1, maior, menor, c2, d2, u2,
+  int n, ninv, x, xinv, y, _case = 0, c1, u1, d1, greater, smaller, c2, d2, u2,
       z;
   for (n = 123; n <=987; n++) {
 
@@ -9,16 +9,16 @@ int main(void) {
     if ((c1!=d1)&&(c1!=u1)&&(u1!=d1)&&(c1!=0)&&(u1 != 0) &&(d1 != 0)){
       ninv = c1 + (10 * d1) + (100 * u1);
       if (n>ninv) {
-        maior = n, menor = ninv;
+        greater = n, smaller = ninv;
       } else {
-        maior = ninv, menor = n;
+        greater = ninv, smaller = n;
       }
-      x=maior-menor;
+      x=greater-smaller;
       c2=x/100, d2=(x%100)/10, u2=(x%100)%10;
       xinv=((c2)+(10*d2)+(100*u2));
       z = x+xinv;
-      caso++;
-     printf("caso %3i: %i-%i=%3i, %3i+%i=%i\n", caso, maior, menor, x, x, xinv, z);
+      _case++;
+     printf("case %3i: %i-%i=%3i, %3i+%i=%i\n", _case, greater, smaller, x, x, xinv, z);
     } else {
     }
   }
